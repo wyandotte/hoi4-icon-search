b314b4b169a00b9fbca4237c9b513b764df3d0c2
wyandotte <github@wyandotte.xyz>
2017-07-02 11:47:45 +0200

    Assets only in docs folder, not in src
    same for index.html

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,32 +1,5 @@
 let mix = require('laravel-mix').mix;
 
-
-// Use a temporary 'staging' folder which shouldn't be included in the repo
-let path = 'staging'
-
-// Only for production we use the 'docs' folder (for GitHub pages)
-if (mix.config.inProduction) {
-  path = 'docs'
-}
-
-mix.copy('src/assets', path + '/assets')
-   .js('src/js/app.js', path + '/assets/')
-   .sass('src/scss/app.scss', path + '/assets')
-   .copy('src/index.html', path + '/')
-   .setPublicPath(path);
-
-
-
-// Full API
-// mix.js(src, output);
-// mix.extract(vendorLibs);
-// mix.sass(src, output);
-// mix.less(src, output);
-// mix.combine(files, destination);
-// mix.copy(from, to);
-// mix.minify(file);
-// mix.sourceMaps(); // Enable sourcemaps
-// mix.version(); // Enable versioning.
-// mix.disableNotifications();
-// mix.setPublicPath('path/to/public'); <-- Useful for Node apps.
-// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+mix.js('src/js/app.js', 'docs/assets/')
+   .sass('src/scss/app.scss', 'docs/assets')
+   .setPublicPath('docs');
